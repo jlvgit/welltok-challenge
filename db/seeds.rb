40339f3e41561afd3e99d5873d9c963c34e2dbee
jvelazquez-sonian <jose.velazquez@sonian.net>
2017-10-28 11:31:22 -0400

    seeding db with csv data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+
+article = []
+
+CSV.foreach('./lib/seeds/import.csv', :headers => true) do |row|
+  row = row.to_hash
+  row.delete("Id")
+  row["tags"] = row["tags"].gsub(/\s+/, "").split(",") if !row["tags"].nil?
+  if row.values.exclude? nil
+    Article.create!(row)
+  else
+    next
+  end
+end
